32f08d12e1a4e5abbf5f0ad506dcb49f22457b30
kebo <kebo2021@qq.com>
2021-09-29 10:04:42 +0800

    Add nvim basic config

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,19 +6,18 @@ vim.cmd[[colorscheme dracula]]
 local function load_options()
     local global_local = {
         mouse = "nv";
-        bg = "dark";
-        ar = true;
-        aw = true;
-        awa = true;
+        background = "dark";
+        autoread = true;
+        autowrite = true;
+        autowriteall= true;
         exrc = true;
+        magic = true;
         secure = true;
         gdefault = true;
         mousefocus = true;
         termguicolors = true;
         virtualedit = "block";
         joinspaces = false;
-        icm = "split";
-        breakindent = true;
         errorbells = false;
         visualbell = false;
         hidden = true;
@@ -27,7 +26,6 @@ local function load_options()
         viewoptions = "folds,cursor,curdir,slash,unix";
         sessionoptions = "curdir,help,tabpages,winsize";
         clipboard = "unnamedplus";
-        wildoptions = "pum";
         wildignorecase = true;
         wildignore = "*.pyc,*.o,*.obj,*.out,*.jpg,*.jpeg,*.png,*.gif,*.zip";
         backup = false;
@@ -37,33 +35,34 @@ local function load_options()
         backupdir = global.cache_dir .. "backup/";
         viewdir = global.cache_dir .. "view/";
         history = 2000;
+        shada = "!,'300,<50,@100,s10,h";
+        smarttab = true;
         timeout = true;
         ttimeout = true;
         timeoutlen = 500;
         ttimeoutlen = 10;
-        updatetime = 300;
+        updatetime = 100;
+        redrawtime = 1500;
         lazyredraw = true;
-        ignorecase = true;
         smartcase = true;
+        ignorecase = true;
         hlsearch = false;
         incsearch = true;
         wrapscan = true;
         grepprg = "rg --hidden --crlf --follow --vimgrep --smart-case --";
         grepformat = "%f:%l:%c:%m";
-        startofline = false;
-        whichwrap = "h,l,<,>,[,],~";
+        -- whichwrap = "h,l,<,>,[,],~";
         splitbelow = true;
         splitright = true;
         eadirection = "hor";
-        foldopen = vim.opt.foldopen + 'search';
-        -- switchbuf = "useopen";
+        switchbuf = "useopen";
         backspace = "indent,eol,start";
         diffopt = "filler,iwhite,internal,algorithm:patience";
         completeopt = "menu,menuone,noselect";
         inccommand = "split";
         jumpoptions = "stack";
-        showmode = false;
         showmatch = true;
+        showmode = false;
         showcmd = false;
         shiftround = true;
         shortmess = {
@@ -71,7 +70,6 @@ local function load_options()
             o = true,
             O = true,
             s = true,
-            t = true,
             T = true,
             W = true,
             A = true,
@@ -80,12 +78,13 @@ local function load_options()
             F = true,
         };
         scrolloff = 1;
-        sidescrolloff = 5;
-        foldlevelstart = 10;
+        sidescrolloff = 1;
+        foldlevelstart = 99;
         ruler = false;
         listchars = {
             eol = nil,
             tab = '··›',
+            lead = '·',
             trail = '•',
             extends = '»',
             precedes = '«'
@@ -94,22 +93,23 @@ local function load_options()
         winwidth = 30;
         winminwidth = 10;
         pumblend = 10;
-        winblend = 10;
         pumheight = 15;
-        helpheight = 12;
+        winblend = 10;
+        helpheight = 15;
         previewheight = 12;
         cmdheight = 1;
         cmdwinheight = 5;
         equalalways = false;
+        display = "lastline";
         laststatus = 2;
         statusline = " %t%r%m %= %y [ %l • %v ] [ %p%% ] ";
     }
 
     local window_local = {
         list = true;
-        scl = "yes:1";
-        nu = true;
-        rnu = true;
+        signcolumn = "yes:1";
+        number = true;
+        relativenumber = true;
         linebreak = true;
         wrap = true;
         conceallevel = 2;
@@ -117,6 +117,7 @@ local function load_options()
         foldenable = true;
         foldmethod = "indent";
         cursorline = false;
+        breakindent = true;
         breakindentopt = "sbr";
         fillchars = {
             vert = ' ',
@@ -131,25 +132,21 @@ local function load_options()
     }
 
     local buffer_local = {
-        swf = false;
-        udf = true;
-        fo = "croql1j";
-        wrapmargin = 2;
+        swapfile = false;
+        undofile = true;
+        formatoptions = "croql1j";
         infercase = true;
         complete = ".,w,b,u,],t";
-        synmaxcol = 2500;
-        textwidth = 100;
-        expandtab = true;
-        autoindent = true;
+        matchpairs = "(:),{:},[:],<:>";
+        synmaxcol = 3000;
+        textwidth = 80;
         tabstop = 4;
         shiftwidth = 4;
         softtabstop = 4;
+        expandtab = true;
+        autoindent = true;
         smartindent = true;
     }
-
-    vim.g.loaded_python_provider = 0
-    vim.g.python3_host_skip_check = 1
-    vim.g.python3_host_prog = '/usr/bin/python3'
 
     for name, value in pairs(global_local) do
         opt[name] = value
