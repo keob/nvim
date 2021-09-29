7070af1b5ee922c254cec402829af22faed5980c
kebo <kebo2021@qq.com>
2021-09-29 10:03:07 +0800

    Add disable plugins and config privider function

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -20,6 +20,32 @@ local createdir = function ()
     end
 end
 
+local disable_distribution_plugins= function()
+    vim.g.loaded_gzip              = 1
+    vim.g.loaded_tar               = 1
+    vim.g.loaded_tarPlugin         = 1
+    vim.g.loaded_zip               = 1
+    vim.g.loaded_zipPlugin         = 1
+    vim.g.loaded_getscript         = 1
+    vim.g.loaded_getscriptPlugin   = 1
+    vim.g.loaded_vimball           = 1
+    vim.g.loaded_vimballPlugin     = 1
+    vim.g.loaded_2html_plugin      = 1
+    vim.g.loaded_logiPat           = 1
+    vim.g.loaded_rrhelper          = 1
+    vim.g.loaded_netrw             = 1
+    vim.g.loaded_netrwPlugin       = 1
+    vim.g.loaded_netrwSettings     = 1
+    vim.g.loaded_netrwFileHandlers = 1
+end
+
+local config_provider = function()
+    vim.g.loaded_python_provider  = 0
+    vim.g.loaded_ruby_provider    = 0
+    vim.g.loaded_perl_provider    = 0
+    vim.g.python3_host_skip_check = 1
+end
+
 local leader_map = function()
     vim.g.mapleader = " "
     vim.api.nvim_set_keymap('n',' ','',{noremap = true})
@@ -27,13 +53,14 @@ local leader_map = function()
 end
 
 local load_core =function()
-    require('pack.plugins')
+    createdir()
     leader_map()
+    config_provider()
+    disable_distribution_plugins()
 
     require('core.options')
+    require('pack.plugins')
     require('keymap')
-
-    createdir()
 end
 
 load_core()
