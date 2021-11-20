f2fc84ad3453fed1055526447e413675c05e477e
kebo <kebo2021@qq.com>
2021-11-20 12:18:55 +0800

    Add dracula colors

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,7 +1,7 @@
 local opt = vim.opt
 local global = require('core.global')
 
-vim.cmd[[colorscheme gruvbox]]
+vim.cmd[[colorscheme dracula]]
 
 local function load_options()
     local global_local = {
